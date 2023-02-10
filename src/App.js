b5b4e17b5b269ea8d263ab3b4dfdfb2746aba7ad
Shivamt2107 <shivam.gote2107@gmail.com>
2023-02-10 08:28:04 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Resume from "./components/Resume/Resume";
+import Contact from "./components/Contact/Contact";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import { createContext } from "react";
+
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const body = document.getElementsByTagName("BODY")[0];
+
+  const changeTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+//282d3f
+  useEffect(() => {
+    if (theme === "dark") {
+      body.style.background = "#282d3f";
+      body.style.color = "#dddddd";
+    } else {
+      body.style.background = "#ffff";
+      body.style.color = "#656d72";
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <h1>Hello World shivam</h1>
-    </div>
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
+      <div className="App" id={theme}>
+        
+        <Navbar />
+        <Home />
+        <About />
+        <Resume />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
